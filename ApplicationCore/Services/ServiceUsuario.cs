0d5025387dfe9a4f5468b53f75a0f249372ac451
anibal.alpizar <anibal.alpizar14@gmail.com>
2023-03-08 13:22:01 -0600

    first commit

---
+++ ApplicationCore/Services/ServiceUsuario.cs
@@ -0,0 +1,40 @@
+﻿
+using Infraestructure.Models;
+using Infraestructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services
+{
+    public class ServiceUsuario : IServiceUsuario
+    {
+        public Usuario GetUsuario(string email, string password)
+        {
+            IRepositoryUsuario repository = new RepositoryUsuario();
+            // Encriptar el password para poder compararlo
+
+            return repository.GetUsuario(email, password);
+        }
+        public Usuario GetUsuarioByID(int id)
+        {
+            IRepositoryUsuario repository = new RepositoryUsuario();
+            Usuario oUsuario = repository.GetUsuarioByID(id);
+            // Desencriptar el password para presentarlo
+
+
+            return oUsuario;
+        }
+        public Usuario Save(Usuario usuario)
+        {
+            IRepositoryUsuario repository = new RepositoryUsuario();
+            // Encriptar el password para guardarlo
+
+
+            return repository.Save(usuario);
+        }
+    }
+}
+
---
+++ Web/Controllers/LoginController.cs
@@ -0,0 +1,65 @@
+﻿using Infraestructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using Web.Utils;
+
+namespace Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Usuario usuario)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, MethodBase.GetCurrentMethod());
+                TempData["Message"] = "Error al procesar los datos! " + ex.Message;
+
+                // Redireccion a la captura del Error
+                return RedirectToAction("Default", "Error");
+            }
+            return View("Index");
+        }
+        public ActionResult UnAuthorized()
+        {
+            ViewBag.Message = "No autorizado";
+            if (Session["User"] != null)
+            {
+                Usuario usuario = Session["User"] as Usuario;
+                Log.Warn($"No autorizado {usuario.Email}");
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            try
+            {
+                //Eliminar variable de sesion
+                
+
+                return RedirectToAction("Index", "Login");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, MethodBase.GetCurrentMethod());
+                TempData["Message"] = "Error al procesar los datos! " + ex.Message;
+
+                // Redireccion a la captura del Error
+                return RedirectToAction("Default", "Error");
+            }
+        }
+    }
+}
